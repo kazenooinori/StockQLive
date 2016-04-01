0f138eb52ddd42a86a5309ecf3df5456025307cb
Daz <daz.lee1987@gmail.com>
2016-04-01 23:12:12 +0800

    refactor user schema

--- server/models/user.js
+++ server/models/user.js
@@ -4,28 +4,15 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: String,
     password: String,
-    provider: {
-        type: String,
-    },
-    displayName: {
-        type: String,
-    },
+    provider: String,
+    displayName: String,
     name: {
         lastName: String,
         firstName: String,
         middleName: String,
     },
-    emails: [
-        {
-            value: String,
-            type: String,
-        }
-    ],
-    photos: [
-        {
-            value: String,
-        }
-    ],
+    emails: String,
+    photos: String,
 });
 
 export default mongoose.model("User", UserSchema);
--- server/routes/login.js
+++ server/routes/login.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const UserStore = require("../stores/user");
 const passport =
 
-router.post("/", passport.authenticate('local'), (req, res) => {
+router.post("/", passport.authenticate('local', {failureFlash: true}), (req, res) => {
     res.json({
         user: req.user
     });
