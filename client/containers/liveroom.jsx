b7cb4ceee6ce9061b4f1dc68b10efc7f2d2b299c
Daz <daz.lee1987@gmail.com>
2016-04-12 18:40:33 +0800

    add demo stock chart

--- client/containers/liveroom.jsx
+++ client/containers/liveroom.jsx
@@ -8,6 +8,7 @@ import Messager from "./messager";
 import CreateChannelModal from "../components/create-channel-modal";
 import {connect} from "react-redux";
 import * as ChaActions from '../actions/cha-actions';
+import Highcharts from 'highcharts/highstock';
 
 const {Component, PropTypes} = React;
 
@@ -39,6 +40,57 @@ const Liveroom = React.createClass({
         onFetchChannels();
 
         this.props.updateStocks();
+
+// for demo stock chart
+        var seriesOptions = [],
+            seriesCounter = 0,
+            names = ['MSFT', 'AAPL', 'GOOG'];
+        $.each(names, function (i, name) {
+            $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+                seriesOptions[i] = {
+                    name: name,
+                    data: data
+                };
+                // As we're loading the data asynchronously, we don't know what order it will arrive. So
+                // we keep a counter and create the chart when all the data is loaded.
+                seriesCounter += 1;
+                if (seriesCounter === names.length) {
+                    createChart();
+                }
+            });
+        });
+        function createChart() {
+            Highcharts.StockChart('stock-chart', {
+               rangeSelector: {
+                  selected: 4
+              },
+
+              yAxis: {
+                  labels: {
+                      formatter: function () {
+                          return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                      }
+                  },
+                  plotLines: [{
+                      value: 0,
+                      width: 2,
+                      color: 'silver'
+                  }]
+              },
+
+              plotOptions: {
+                  series: {
+                      compare: 'percent'
+                  }
+              },
+
+              tooltip: {
+                  pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                  valueDecimals: 2
+              },
+              series: seriesOptions
+           });
+        }
     },
     renderStockItems (stocks) {
         return stocks.filter(checkLatestPrice).map((stock) => {
@@ -73,6 +125,9 @@ const Liveroom = React.createClass({
                         channel={channel}
                         socket={socket}/>
                     <div className="tail">
+                        <div id="stock-chart">
+
+                        </div>
                         <div className="ui tab segment board active">
                             <div className="stock-list ui middle aligned selection list">
                                 {this.renderStockItems(stocks)}
