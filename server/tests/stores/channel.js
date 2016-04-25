96b95d196b01e71979c62f142aedefdd9cc4472e
Daz <daz.lee1987@gmail.com>
2016-04-25 21:15:06 +0800

    use sinon to represent mock object

--- server/tests/stores/channel.js
+++ server/tests/stores/channel.js
@@ -1,15 +1,41 @@
 const mocha = require("mocha");
+const sinon = require("sinon");
 const {assert, expect} = require("chai");
-const channelStore = require("../../stores/channel");
+const ChannelModel = require("../../models/channel");
+const ChannelStore = require("../../stores/channel");
 
 describe("server/stores/channel.js", () => {
+    var stub;
     before(() => {
-        
+        stub = sinon.stub(ChannelModel, "create", (attributes, callback) => {
+            const result = Object.assign({}, attributes, {
+                createdAt: "2016-01-01T00:00:00Z",
+                updatedAt: "2016-01-01T00:00:00Z",
+            });
+            callback(null, result);
+        });
     });
+    after(() => {
+        stub.restore();
+    });
+
     describe("findChannels", () => {
-        it("should fin channels", (done) => {
-            //expect().equal(-1, [1,2,3].indexOf(5));
-            done();
+        it("should create channel", (done) => {
+            const channel = {
+                name: "test channel",
+                ownerUsername: "dazlee",
+                chatroomId: "chatroomId",
+                type: "type",
+            };
+            const result = Object.assign({}, channel, {
+                createdAt: "2016-01-01T00:00:00Z",
+                updatedAt: "2016-01-01T00:00:00Z",
+            });
+            ChannelStore.createChannel(channel)
+            .then((createdChannel) => {
+                expect(createdChannel).to.deep.equal(result);
+                done();
+            });
         });
     });
 });
