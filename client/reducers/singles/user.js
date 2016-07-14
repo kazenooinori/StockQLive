f05efbcc7d1cb2dd2d137698da2212bb5026d652
Daz <daz.lee1987@gmail.com>
2016-07-14 15:01:22 +0800

    reset initial user

--- client/reducers/singles/user.js
+++ client/reducers/singles/user.js
@@ -1,11 +1,17 @@
 import * as types from "../../constants/action-types";
 import {Map} from "immutable";
 
+// const initialState = Map({
+//     _id: "123456",
+//     username: "123456",
+//     displayName: "Dazlee",
+//     photos: "https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-1/c8.0.80.80/p80x80/10153880_10202861108760535_1171467779_n.jpg?oh=4f01035bebe96f5502ef38b2d81d9727&oe=57EF3664",
+// });
 const initialState = Map({
-    _id: "123456",
-    username: "123456",
-    displayName: "Dazlee",
-    photos: "https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-1/c8.0.80.80/p80x80/10153880_10202861108760535_1171467779_n.jpg?oh=4f01035bebe96f5502ef38b2d81d9727&oe=57EF3664",
+    _id: "",
+    username: "",
+    displayName: "",
+    photos: "",
 });
 export default function user (state = initialState, action) {
     switch (action.type) {
